f60b186f38136047e5481f98960cb5bc0fb17e95
Codeuxe <codeuxe@gmail.com>
2025-10-14 17:00:20 +0000

    add more brands and types of cars in cars page.

--- src/lib/car-data.ts
+++ src/lib/car-data.ts
@@ -1,5 +1,5 @@
 
-export const vehicleBrands = ['Audi', 'BMW', 'Chevrolet', 'Ford', 'Honda', 'Hyundai', 'Jeep', 'Kia', 'Land Rover', 'Lexus', 'Mercedes-Benz', 'Nissan', 'Porsche', 'Subaru', 'Toyota', 'Volkswagen', 'Volvo'].sort();
-export const vehicleTypes = ['Sedan', 'SUV', 'Truck', 'Coupe', 'Convertible', 'Hatchback', 'Minivan'].sort();
+export const vehicleBrands = ['Acura', 'Alfa Romeo', 'Aston Martin', 'Audi', 'Bentley', 'BMW', 'Bugatti', 'Buick', 'Cadillac', 'Chevrolet', 'Chrysler', 'Dodge', 'Ferrari', 'Fiat', 'Ford', 'Genesis', 'GMC', 'Honda', 'Hyundai', 'Infiniti', 'Jaguar', 'Jeep', 'Kia', 'Lamborghini', 'Land Rover', 'Lexus', 'Lincoln', 'Lotus', 'Maserati', 'Mazda', 'McLaren', 'Mercedes-Benz', 'Mini', 'Mitsubishi', 'Nissan', 'Polestar', 'Porsche', 'Ram', 'Rivian', 'Rolls-Royce', 'Subaru', 'Tesla', 'Toyota', 'Volkswagen', 'Volvo'].sort();
+export const vehicleTypes = ['Sedan', 'SUV', 'Truck', 'Coupe', 'Convertible', 'Hatchback', 'Minivan', 'Wagon', 'Van'].sort();
 export const vehicleYears = Array.from({length: 30}, (_, i) => new Date().getFullYear() - i);
 export const locations = ['USA', 'UK', 'UAE', 'CANADA', 'INDIA', 'CHINA', 'JAPAN'];
