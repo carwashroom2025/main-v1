870a6be74323bf536f9de4567dc7ab7928dcdbe9
Carwashroom <carwashroom2025@gmail.com>
2025-10-23 00:14:24 +0000

    reduce the height of footer section  (_for element <Footer>_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -41,7 +41,8 @@ const FacebookIcon = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 const YoutubeIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg style={{enableBackground:"new 0 0 64 64"}} version="1.1" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg style={{enableBackground:"new 0 0 64 64"}} version="1.1" viewBox="0 0 64 64" xmlns="http://www
+.w3.org/2000/svg" {...props}>
         <path d="M50,52H14C7.4,52,2,46.6,2,40V24c0-6.6,5.4-12,12-12h36c6.6,0,12,5.4,12,12v16C62,46.6,56.6,52,50,52z" fill="#C2191E"/>
         <polygon points="24,42 24,22 44,32  " fill="#FFFFFF"/>
     </svg>
